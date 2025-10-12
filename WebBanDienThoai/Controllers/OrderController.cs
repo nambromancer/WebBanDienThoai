683c8753772deafc7b68e6aecd0c84bdbb8a274b
nambromancer <vunguyennam03@gmail.com>
2025-10-13 01:36:37 +0700

    Cập nhật thêm giao diện trang địa chỉ giao hàng, thanh toán và xác nhận đơn hàng

---
+++ WebBanDienThoai/Controllers/OrderController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebBanDienThoai.Controllers
+{
+    public class OrderController : Controller
+    {
+        public ActionResult DeliveryAddress()
+        {
+            return View();
+        }
+
+        public ActionResult Payment()
+        {
+            return View();
+        }
+
+        public ActionResult OrderConfirmed()
+        {
+            return View();
+        }
+    }
+}
