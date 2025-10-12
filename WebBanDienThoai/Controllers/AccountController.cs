a95e8220323b9d9531c953bf09ecbdc5c73e1f36
nambromancer <vunguyennam03@gmail.com>
2025-10-12 21:29:45 +0700

    Thêm đăng ký, đăng nhập cho website

---
+++ WebBanDienThoai/Controllers/AccountController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebBanDienThoai.Models;
+
+namespace WebBanDienThoai.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: /Account/Register
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View(new RegisterViewModel());
+        }
+
+        // POST: /Account/Register
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(RegisterViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            // TODO: save user later. For now just redirect to the main page (layout).
+            TempData["Toast"] = "Đăng ký thành công.";
+            return RedirectToAction("Index", "Home"); // or: return Redirect("~/");
+        }
+
+        // Optional: Login
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View(new LoginViewModel());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid) return View(model);
+
+            TempData["Toast"] = "Đăng nhập (frontend).";
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
